746998925548c370e6a89b314a42a2ef9ae7866b
mindy <mindytai175@gmail.com>
2020-11-27 00:01:17 +0800

    add login button

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Button } from 'react-native';
 
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
@@ -23,7 +24,18 @@ function App() {
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={ HomeScreen } />
+          <Stack.Screen 
+            name="Home" 
+            component={ HomeScreen }
+            options={{
+              headerRight: ()=> (
+                <Button 
+                  onPress={() => alert('This is a button!')}
+                  title="Login"
+                />
+              )
+            }} 
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
